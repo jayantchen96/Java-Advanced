92185c4d9a1fa57f1fa98de69ba97e1f545ede60
JayantChen <jayantchencoder@gmail.com>
2021-04-25 11:40:54 +0800

    week06 sql

--- week06/src/emall.sql
+++ week06/src/emall.sql
@@ -6,29 +6,29 @@ create database if not exists emall;
 use emall;
 # 创建用户表
 create table `user` (
-                        `id` int auto_increment not null comment '用户ID',
-                        `username` varchar(32) not null comment '用户名',
-                        `password` varchar(32) not null comment '密码',
-                        `nickname` varchar(32) comment '昵称',
-                        `idCard` varchar(17) unique not null comment '身份证',
-                        primary key (`id`)
+    `id` int auto_increment not null comment '用户ID',
+    `username` varchar(32) not null comment '用户名',
+    `password` varchar(32) not null comment '密码',
+    `nickname` varchar(32) comment '昵称',
+    `idCard` varchar(17) unique not null comment '身份证',
+    primary key (`id`)
 )engine=Innodb default charset=utf8;
 
 # 创建商品表
 create table `product` (
-                           `id` int auto_increment not null comment '商品ID',
-                           `name` varchar(32) not null comment '商品名',
-                           `category` varchar(32) comment '商品类别',
-                           `weight` decimal(16, 2) unique not null comment '商品重量',
-                           primary key (`id`)
+    `id` int auto_increment not null comment '商品ID',
+    `name` varchar(32) not null comment '商品名',
+    `category` varchar(32) comment '商品类别',
+    `weight` decimal(16, 2) unique not null comment '商品重量',
+     primary key (`id`)
 )engine=Innodb default charset=utf8;
 
 # 创建订单表
 create table `order` (
-                         `id` int not null comment '订单ID',
-                         `userId` int not null comment '用户ID',
-                         `amount` decimal(16, 2) comment '金额',
-                         `status` varchar(10) unique not null comment '订单状态',
-                         primary key (`id`),
-                         foreign key (`userId`) references user(`id`)
+    `id` int not null comment '订单ID',
+    `userId` int not null comment '用户ID',
+    `amount` decimal(16, 2) comment '金额',
+    `status` varchar(10) unique not null comment '订单状态',
+    primary key (`id`),
+    foreign key (`userId`) references user(`id`)
 )engine=Innodb default charset=utf8;
